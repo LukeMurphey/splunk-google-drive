a80613b323c1edd65855a7bcbdea89e1026d9f86
Luke Murphey <Luke@LukeMurphey.net>
2015-01-19 04:48:14 -0600

    Uploading initial version of the app

---
+++ src/bin/google_drive_app/__init__.py
@@ -0,0 +1,272 @@
+"""
+This module includes classes necessary to export and import lookups to Google spreadsheets.
+
+
+from google_drive_app import GoogleLookupSync
+
+completed = GoogleLookupSync.import_to_lookup_file("etc/apps/search/lookups/some_lookup_file.csv", "Survey Results", "raw_data", GoogleLookupSync.OperationAction.OVERWRITE)
+
+if completed:
+    print "import was successful!"
+
+"""
+
+import os
+import csv
+import shutil
+import logging
+import lookupfiles
+import gspread
+from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
+
+class GoogleLookupSync(object):
+    """
+    This class performs operation for importing content from Google Drive to Splunk.
+    """
+    
+    class OperationAction:
+        OVERWRITE = 1
+        APPEND    = 2
+        
+    class Operation:
+        IMPORT      = 1
+        EXPORT      = 2
+        SYNCHRONIZE = 3
+        
+    def __init__(self, login=None, password=None, oauth2credentials=None):
+        
+        self.gspread_client = None
+                
+        if oauth2credentials is not None:
+            self.authenticate_by_oauth2()
+        else:
+            self.authenticate_by_password(login, password)
+            
+        self.logger = None
+        self.get_logger()
+        
+    def authenticate_by_password(self, login, password):
+        """
+        Authenticate to Google and initialize a gspread client.
+        
+        Args:
+          login (str): The login to use for authenticating to Google
+          password (str): The password to use for authenticating to Google
+        """
+        
+        # Make sure a login name and password were provided
+        if login is None or password is None:
+            raise ValueError("Neither a login name nor a password was provided")
+        
+        self.gspread_client = gspread.login(login, password)
+        return self.gspread_client
+        
+    def authenticate_by_oauth2(self, oauth2credentials):
+        """
+        Authenticate to Google and initialize a gspread client using OAuth2
+        
+        Args:
+          oauth2credentials (OAuth2Credentials): The oauth2credentials to use for authenticating.
+        """
+        
+        self.gspread_client = gspread.authorize(oauth2credentials)
+        return self.gspread_client
+    
+    def open_google_spreadsheet(self, title=None, key=None):
+        """
+        Open the spreadsheet with the given title or key, Either the key or title must be provided.
+        
+        If both are provided, then the title will be used. If a sheet could not be opened with the title, then the key will be used.
+        
+        Args:
+          title (str, optional): The title of the document
+          key (str, optional): The key of the document
+          
+        Returns:
+          The Google spreadsheet object
+        """
+        
+        if title is None and key is None:
+            raise ValueError("You must supply either the title or the key of the sheet you want to open")
+        
+        google_spread_sheet = None
+        
+        # Try to open the file by the title
+        if title is not None:
+            google_spread_sheet = self.gspread_client.open(title)
+            
+            
+        # If we don't have the sheet yet, try using the key
+        if google_spread_sheet is None and key is not None:
+            self.gspread_client.open_by_key(key)
+        
+        return google_spread_sheet
+    
+    def get_lookup_file_row_count(self, lookup_file_path):
+        pass
+    
+    def copy_file(self, src, dst):
+        pass
+    
+    def get_logger(self):
+        """
+        Setup a logger for this class.
+        
+        Returns:
+          A logger
+        """
+    
+        try:
+            return logger
+        except:
+            pass
+        
+        if self.logger is not None:
+            return self.logger
+    
+        logger = logging.getLogger('splunk.google_drive.GoogleLookupSync')
+        logger.setLevel(logging.DEBUG)
+        #logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
+    
+        #file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', 'google_lookup_sync.log']), maxBytes=25000000, backupCount=5)
+    
+        #formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        #file_handler.setFormatter(formatter)
+        #logger.addHandler(file_handler)
+        
+        self.logger = logger
+        return logger
+    
+    def import_to_lookup_file_by_transform(self, lookup_transform, namespace, owner, google_spread_sheet_name, worksheet_name, session_key, create_if_non_existent=False):
+        """
+        Import the spreadsheet from Google to the given lookup file.
+        
+        Args:
+          lookup_transform (str): The name of the lookup file transform to write 
+          namespace (str): 
+          owner (str): 
+          google_spread_sheet_name (str): 
+          worksheet_name (str): 
+          session_key (str): 
+          create_if_non_existent (bool, optional): Defaults to False.
+        """
+        transform = lookupfiles.get_lookup_table_location(lookup_transform, namespace, owner, session_key, True)
+        
+        self.import_to_lookup_file(transform.filename, namespace, owner, google_spread_sheet_name, worksheet_name, session_key, create_if_non_existent)
+    
+    def import_to_lookup_file(self, lookup_name, namespace, owner, google_spread_sheet_name, worksheet_name, session_key, create_if_non_existent=False):
+        """
+        Import the spreadsheet from Google to the given lookup file.
+        
+        Args:
+          lookup_name (str): The name of the lookup file to write (not the full path, just the stanza name)
+          namespace (str): 
+          owner (str): 
+          google_spread_sheet_name (str): 
+          worksheet_name (str): 
+          session_key (str): 
+          create_if_non_existent (bool, optional): Defaults to False.
+        """
+        
+        destination_full_path = lookupfiles.SplunkLookupTableFile.get(lookupfiles.SplunkLookupTableFile.build_id(lookup_name, namespace, owner), sessionKey=session_key).path
+        
+        if destination_full_path is None and not create_if_non_existent:
+            raise Exception("Lookup file to import into does not exist")
+        
+        elif create_if_non_existent:
+            # TODO handle user-based lookups
+            destination_full_path = make_splunkhome_path(['etc', 'apps', namespace, 'lookups', lookup_name])
+        
+        self.import_to_lookup_file_full_path(destination_full_path, namespace, owner, google_spread_sheet_name, worksheet_name, session_key, create_if_non_existent, lookup_name=lookup_name)
+        
+    def import_to_lookup_file_full_path(self, destination_full_path, namespace, owner, google_spread_sheet_name, worksheet_name, session_key, create_if_non_existent=False, lookup_name=None):
+        """
+        Import the spreadsheet from Google to the given lookup file.
+        
+        Args:
+          destination_full_path (str): The full path of the file to write out
+          namespace (str): 
+          owner (str): 
+          google_spread_sheet_name (str): 
+          worksheet_name (str): 
+          session_key (str):
+          lookup_name (str): The name of the lookup file to write (not the full path, just the stanza name). This is necessary to use Splunk's safe method of copying.
+          create_if_non_existent (bool, optional): Defaults to False.
+        """
+        
+        # Open the spreadsheet
+        google_spread_sheet = self.open_google_spreadsheet(google_spread_sheet_name)
+        
+        # Open or make the worksheet
+        google_work_sheet = self.get_or_make_sheet_if_necessary(google_spread_sheet, worksheet_name)
+        
+        # Make a temporary lookup file
+        temp_file = lookupfiles.get_temporary_lookup_file()
+        
+        # Get the contents of spreadsheet and import it into the lookup
+        list_of_lists = google_work_sheet.get_all_values()
+        
+        with temp_file: #open(temp_file.name, 'w') as temp_file_handle:
+            
+            # Open a CSV writer to edit the file
+            csv_writer = csv.writer(temp_file, lineterminator='\n')
+            
+            for row in list_of_lists:
+                
+                # Update the CSV with the row
+                csv_writer.writerow(row)
+                
+        # Determine if the lookup file exists, create it if it doesn't
+        lookup_file_exists = os.path.exists(destination_full_path)
+        
+        if not lookup_file_exists or lookup_name is None:
+            
+            # If we are not allowed to make the lookup file, then throw an exception
+            if not create_if_non_existent:
+                raise Exception("The lookup file to import the content to does not exist")
+            
+            # Manually copy the file to create it
+            if lookup_file_exists:
+                shutil.copy(temp_file.name, destination_full_path)
+            else:
+                shutil.move(temp_file.name, destination_full_path)
+                
+            self.get_logger().info('Lookup created successfully, user=%s, namespace=%s, lookup_file=%s', owner, namespace, lookup_name)
+            
+            # If the file is new, then make sure that the list is reloaded so that the editors notice the change
+            lookupfiles.SplunkLookupTableFile.reload()
+            
+        # Edit the existing lookup otherwise
+        else:
+            
+            # Default to nobody if the owner is None
+            if owner is None:
+                owner = "nobody" 
+            
+            # Persist the changes from the temporary file
+            print "\n\nlookupfiles.update_lookup_table(", temp_file.name, lookup_name, namespace, owner, session_key, ")"
+            
+            lookupfiles.update_lookup_table(filename=temp_file.name, lookup_file=lookup_name, namespace=namespace, owner=owner, key=session_key)
+            
+    
+    def get_or_make_sheet_if_necessary(self, google_spread_sheet, worksheet_name, rows=100, cols=20):
+        """
+        Create the worksheet in the given Google document if it does not exist. If it does, return it. 
+        
+        Args:
+          google_spread_sheet_name (str): 
+          worksheet_name (str): 
+          rows (int, optional): Defaults to 100.
+          cols (int, optional): Defaults to 20.
+          
+        Returns:
+          The Google worksheet object
+        """
+        
+        try:
+            worksheet = google_spread_sheet.worksheet(worksheet_name)
+        except gspread.WorksheetNotFound:
+            # Worksheet was not found, make it
+            worksheet = google_spread_sheet.add_worksheet(title=worksheet_name, rows=str(rows), cols=str(cols))
+            
+        return worksheet
---
+++ tests/test_google_drive_app.py
@@ -0,0 +1,130 @@
+# coding=utf-8
+import unittest
+import sys
+import os
+import time
+import re
+import splunk.auth
+
+sys.path.append( os.path.join("..", "src", "bin") )
+sys.path.append( os.path.join("..", "src", "bin", "google_drive_app") )
+
+from google_drive_app import GoogleLookupSync, gspread, lookupfiles
+
+class SplunkGoogleDriveTestCase(unittest.TestCase):
+    
+    def get_property(self, pattern, file_contents, exception_msg):
+        
+        m = re.search(pattern, file_contents)
+        
+        if m is None:
+            raise Exeption(exception_msg)
+        else:
+            value = m.groups()[0]
+            
+            return value
+    
+    def get_login_and_password(self):
+        
+        login = None
+        password = None
+        
+        props_fh = open('../local.properties', 'r')
+        
+        props = props_fh.read()
+        #props_fh.readlines()
+        
+        # Find the login and password entries
+        login = self.get_property("value[.]test[.]google_login[ ]*[=](.*)", props, "Google login was not specified")
+        password = self.get_property("value[.]test[.]google_password[ ]*[=](.*)", props, "Google password was not specified")
+        
+        return login, password
+    
+    def get_google_lookup_sync_instance(self):
+        
+        login, password = self.get_login_and_password()
+        
+        return GoogleLookupSync(login, password)
+    
+    def setUp(self):
+        self.google_lookup_sync = self.get_google_lookup_sync_instance()
+
+class TestLookupImport(SplunkGoogleDriveTestCase):
+
+    def test_no_auth_creds(self):
+        
+        def make_client_no_creds():
+            GoogleLookupSync()
+        
+        self.assertRaises(ValueError, make_client_no_creds)
+
+    def test_open_google_spreadsheet_no_title_no_key(self):
+        self.assertRaises(ValueError, self.google_lookup_sync.open_google_spreadsheet)
+    
+    def test_open_google_spreadsheet_by_title(self):
+        self.google_lookup_sync.open_google_spreadsheet(title="test_case_import")
+        
+    def test_open_google_spreadsheet_by_key(self):
+        self.google_lookup_sync.open_google_spreadsheet(key="12lGRqELJlj9osKD08ASKO9B6A2G2SqOtrvHh4OR4-sA")
+ 
+    def test_get_or_make_sheet_if_necessary(self):
+        
+        google_spread_sheet = self.google_lookup_sync.open_google_spreadsheet(title="test_case_make_worksheet")
+        worksheet_name = "created_by_unit_test"
+        
+        # Try to get the worksheet and delete it if it already exists
+        try:
+            worksheet = google_spread_sheet.worksheet(worksheet_name)
+            google_spread_sheet.del_worksheet(worksheet)
+        except gspread.WorksheetNotFound:
+            pass
+        
+        # Make it
+        self.google_lookup_sync.get_or_make_sheet_if_necessary(google_spread_sheet, worksheet_name)
+    
+        # Confirm that the sheet exists (if this throws an exception, then the test failed)
+        worksheet = google_spread_sheet.worksheet(worksheet_name)
+        
+        # Now delete it for future tests
+        google_spread_sheet.del_worksheet(worksheet)
+        
+    def test_import_by_full_lookup_path(self):
+        
+        session_key = splunk.auth.getSessionKey(username='admin', password='changeme')
+        namespace = "search"
+        owner = "nobody"
+        created_file_path = None
+        
+        with lookupfiles.get_temporary_lookup_file() as temp_file:
+            
+            created_file_path = temp_file.name
+            
+            # Import the file
+            self.google_lookup_sync.import_to_lookup_file_full_path(created_file_path, namespace, owner, "test_case_import", "data", session_key, create_if_non_existent=True)
+        
+        # Check the results    
+        self.assertTrue(os.path.exists(created_file_path), "Lookup file did not get created")
+        print 'File imported successfully, size=%r, path=%r' % (os.path.getsize(created_file_path), created_file_path)
+        self.assertEquals(os.path.getsize(created_file_path), 72, "Lookup file was not populated correctly")
+        
+    def test_import_by_lookup_name(self):
+        session_key = splunk.auth.getSessionKey(username='admin', password='changeme')
+        lookup_name = "test_case_import.csv"
+        namespace = "search"
+        owner = "admin"
+        
+        # Import the file
+        #self.google_lookup_sync.import_to_lookup_file(lookup_name, namespace, owner, google_spread_sheet_name, worksheet_name, session_key, create_if_non_existent)
+        self.google_lookup_sync.import_to_lookup_file(lookup_name, namespace, owner, "test_case_import", "data", session_key, create_if_non_existent=False)
+        
+        # Check the file
+        destination_full_path = lookupfiles.get_lookup_table_location(lookup_name, namespace, owner, session_key, True)
+        
+        self.assertTrue(os.path.exists(destination_full_path), "File to import was not created properly")
+        
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suites = []
+    suites.append(loader.loadTestsFromTestCase(TestLookupImport))
+    
+    unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(suites))
