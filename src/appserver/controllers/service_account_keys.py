f452e6d8c4e48812db356148f86f454d4ec8d85f
Luke Murphey <Luke@LukeMurphey.net>
2016-01-19 01:35:31 -0600

    Adding setup screen that allows setting of service account key

---
+++ src/appserver/controllers/service_account_keys.py
@@ -0,0 +1,172 @@
+import logging
+import os
+import sys
+import json
+import cherrypy
+import urllib
+import re
+import base64
+
+from splunk import AuthorizationFailed, ResourceNotFound
+import splunk.appserver.mrsparkle.controllers as controllers
+import splunk.appserver.mrsparkle.lib.util as util
+from splunk.appserver.mrsparkle.lib import jsonresponse
+from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
+import splunk.clilib.bundle_paths as bundle_paths
+from splunk.util import normalizeBoolean as normBool
+from splunk.appserver.mrsparkle.lib.decorators import expose_page
+from splunk.appserver.mrsparkle.lib.routes import route
+import splunk.entity as entity
+
+def setup_logger(level):
+    """
+    Setup a logger for the REST handler.
+    """
+
+    logger = logging.getLogger('splunk.appserver.google_drive.controllers.ServiceAccountKeys')
+    logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
+    logger.setLevel(level)
+
+    file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', 'google_drive_service_account_keys_controller.log']), maxBytes=25000000, backupCount=5)
+
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+logger = setup_logger(logging.DEBUG)
+
+class ServiceAccountKeys(controllers.BaseController):
+    '''
+    A controller for managing Google OAuth2 service account keys
+    '''
+ 
+    DEFAULT_NAMESPACE ="google_drive"
+    DEFAULT_OWNER = "nobody"
+ 
+    def is_file_name_valid(self, lookup_file):     
+        """
+        Indicate if the lookup file is valid (doesn't contain invalid characters such as "..").
+        """
+         
+        allowed_path = re.compile("^[-A-Z0-9_ ]+([.][-A-Z0-9_ ]+)*$", re.IGNORECASE)
+        
+        if not allowed_path.match(lookup_file):
+            return False
+        else:
+            return True
+    
+    def render_error_json(self, msg):
+        output = jsonresponse.JsonResponse()
+        output.data = []
+        output.success = False
+        output.addError(msg)
+        cherrypy.response.status = 400
+        return self.render_json(output, set_mime='text/plain')
+ 
+    def setKeyForDefaultInput(self, file_name):
+        
+        entity_entry = self.getDefaultGoogleDriveInputEntity()
+        
+        session_key = cherrypy.session.get('sessionKey')
+        
+        entity_entry['service_account_key_file'] = file_name
+        entity_entry.namespace = ServiceAccountKeys.DEFAULT_NAMESPACE
+        entity_entry.owner = ServiceAccountKeys.DEFAULT_OWNER
+        entity.setEntity(entity_entry, sessionKey=session_key)
+ 
+    def getClientEmailFromKeyFile(self, file_name):
+        
+        service_key_file_path = make_splunkhome_path(['etc', 'apps', 'google_drive', 'service_account_keys', file_name])
+        service_key = None
+        
+        try:
+            with open(service_key_file_path, 'r') as fh:
+                service_key = json.load(fh)
+                
+                if 'client_email' in service_key:
+                    return service_key['client_email']
+                
+        except IOError:
+            # File could not be loaded
+            return None
+            
+        return None
+        
+    def getDefaultGoogleDriveInputEntity(self):
+        session_key = cherrypy.session.get('sessionKey')
+        return entity.getEntity('admin/conf-inputs', 'google_spreadsheet', sessionKey=session_key)
+        
+    @expose_page(must_login=True, methods=['GET']) 
+    def getDefaultServiceAccountKeyInfo(self, **kwargs):
+        
+        file_name = None
+        service_account_email = None
+        
+        # Get the existing service key
+        session_key = cherrypy.session.get('sessionKey')
+        default_password_entry = self.getDefaultGoogleDriveInputEntity()
+        
+        # Make sure the file name is specified in the default entry
+        if 'service_account_key_file' in default_password_entry:
+            file_name = default_password_entry['service_account_key_file']
+        
+            service_account_email = self.getClientEmailFromKeyFile(file_name)
+        
+        # Return the information
+        return self.render_json({
+                                 'filename' : file_name,
+                                 'service_account_email' : service_account_email
+                                 })
+ 
+    @expose_page(must_login=True, methods=['POST']) 
+    def uploadServiceAccountKey(self, file_contents, file_name):
+        
+        # Ensure that the file name is valid
+        if not self.is_file_name_valid(file_name):
+            return self.render_error_json(_("The service account key filename contains disallowed characters"))
+        
+        # Decode the content (it is sent as a base64 encoded file)
+        file_contents_decoded = base64.b64decode(file_contents.split(',')[1])
+        
+        # Parse the key and make sure it is valid
+        service_account_email = None
+        
+        try:
+            account_key = json.loads(file_contents_decoded)
+            
+            # Stop if the client email address is not included
+            if 'client_email' not in account_key:
+                return self.render_error_json(_("The service account key did not include a client email address"))
+            
+            # Get the email address
+            else:
+                service_account_email = account_key['client_email']
+            
+        except ValueError as e:
+            return self.render_error_json(_("The service account key filename is invalid"))
+        
+        # Create the service account keys directory if it does not yet exist
+        try:
+            os.mkdir(make_splunkhome_path(['etc', 'apps', 'google_drive', 'service_account_keys']))
+        except OSError as e:
+            if e.errno == 17:
+                pass # Path already exists, thats ok
+            else:
+                raise e
+        
+        # Write out the file
+        service_key_file_path = make_splunkhome_path(['etc', 'apps', 'google_drive', 'service_account_keys', file_name])
+        
+        with open(service_key_file_path, 'w') as fh:
+            fh.write(file_contents_decoded)
+        
+        # Set the input such that this key is used
+        self.setKeyForDefaultInput(file_name)
+        
+        # Return the information
+        return self.render_json({
+                                 'filename' : file_name,
+                                 'service_account_email' : service_account_email
+                                 })
+        
---
+++ src/appserver/static/google_drive_setup.js
@@ -0,0 +1,236 @@
+
+require(['jquery','underscore','splunkjs/mvc', 'jquery', 'splunkjs/mvc/simplesplunkview', 'splunkjs/mvc/simplexml/ready!'],
+	function($, _, mvc, $, SimpleSplunkView){
+		
+	    // Define the setup view class
+	    var GoogleDriveSetupView = SimpleSplunkView.extend({
+	        className: "GoogleDriveSetupView",
+	        
+	        defaults: {
+	        	filename: null,
+	        	service_account_email: null
+	        },
+	        
+	        events: {
+	        	"click #choose-import-file" : "chooseImportFile",
+	        	"change #import-file-input" : "handleUploadedFile" //handleUploadedFile
+	        },
+	        
+	        initialize: function() {
+	        	this.options = _.extend({}, this.defaults, this.options);
+	        	
+	        	this.filename = this.options.filename;
+	        	this.service_account_email = this.options.service_account_email;
+	        },
+	        
+	        /**
+	         * For some reason the backbone handlers don't work.
+	         */
+	        setupDragDropHandlers: function(){
+	        	
+	        	// Setup a handler for handling files dropped on the import dialog
+	        	drop_zone2 = document.getElementById('import-file-modal');
+	        	this.setupDragDropHandlerOnElement(drop_zone2);
+	        	
+	        },
+	        
+	        /**
+	         * Setup drag & drop handlers on the given drop-zone.
+	         */
+	        setupDragDropHandlerOnElement: function(drop_zone){
+	        	
+	        	drop_zone.ondragover = function (e) {
+	        		e.preventDefault();
+	        		e.dataTransfer.dropEffect = 'copy';
+	        	}.bind(this);
+	        	
+	        	drop_zone.ondrop = function (e) {
+	        	      e.preventDefault();
+	        	      this.handleUploadedFile(e);
+	        	      return false;
+	        	}.bind(this);
+	        },
+	        
+	        /**
+	         * Upload the given file to Splunk
+	         */
+	        uploadFile: function (file, file_content) {
+	        	
+	        	var promise = jQuery.Deferred();
+	        	
+	        	var file_uri = Splunk.util.make_url("/custom/google_drive/service_account_keys/uploadServiceAccountKey")
+	        	var file_name = file.name;
+
+	        	var data = {
+	        		'file_name' : file_name,
+	        		'file_contents' : file_content
+	        	}
+	           
+	        	jQuery.ajax({
+		             url: file_uri,
+		     		 data: data,
+	        		 type: 'POST',
+		             async: false,
+		             
+		             success: function(result) {
+		            	 console.info("Successfully added a service account key file");
+		            	 promise.resolve(file_name);
+		             }.bind(this),
+		             
+		             error: function(jqXHR,textStatus,errorThrown) {
+		            	 console.error("Unable to save the file");
+		            	 promise.reject("Unable to save the file");
+		             }
+	        	});
+
+	        	return promise;
+	        },
+	        
+	        /**
+	         * Handle the event that occurs when a file is uploaded
+	         */
+	        handleUploadedFile: function(evt){
+	        	
+	        	// Stop if the browser doesn't support processing files in Javascript
+	        	if(!window.FileReader){
+	        		alert("Your browser doesn't support file reading in Javascript; thus, I cannot parse your uploaded file");
+	        		return false;
+	        	}
+
+	        	var files = [];
+
+	        	// Get the files from an input widget if available
+	        	if(evt.target.files && evt.target.files.length > 0){
+	        		files = evt.target.files;
+	        	}
+
+	        	// Get the files from the drag & drop if available
+	        	else if(evt.dataTransfer && evt.dataTransfer.files.length > 0){
+	        		files = evt.dataTransfer.files;
+	        	}
+
+	        	// Stop if no files where provided (user likely pressed cancel)
+	        	if(files.length > 0 ){
+
+	        		// Get the file name
+	        		var file_size = files[0].size;
+
+	        		// Check if file is larger than 4MB
+	        		if (file_size > 4194304) {
+	        			alert("File is larger than 4MB");
+	        			return false;
+	        		}
+
+	        		// Get a reader so that we can read in the file
+	        		var reader = new FileReader();
+
+	        		// Setup an onload handler that will process the file
+	        		reader.onload = function(evt) {
+	        			
+	        			// Stop if the ready state isn't "loaded"
+	        			if(evt.target.readyState != 2){
+	        				return;
+	        			}
+	        			
+	        			// Stop if the file could not be processed
+	        			if(evt.target.error) {
+	        				alert("File could not be processed");
+	        				return;
+	        			}
+	        			
+	        			// Get the file contents
+	        			var file_content = evt.target.result;
+	        			
+	        			// Upload the file contents
+	        			$.when(this.uploadFile(files[0], file_content)).done(function(file_name){
+	        				console.info("File successfully uploaded");
+	        			});
+
+	        		}.bind(this);
+
+	             	// Start the process of processing file
+	             	reader.readAsDataURL(files[0]);
+	        	}
+	        	else{
+	        	   alert("No file object was found");
+	        	   return false;
+	           	}
+	        	
+	        	return true;
+	        },
+	        
+	        /**
+	         * Open the file dialog to select a file to import.
+	         */
+	        chooseImportFile: function(){
+	        	$("#import-file-input").click();
+	        },
+	        
+	        /**
+	         * Import the dropped file.
+	         */
+	        onDropFile: function(evt){
+	        	
+	        	console.log("Got a file via drag and drop");
+	        	evt.stopPropagation();
+	            evt.preventDefault();
+	            var files = evt.dataTransfer.files;
+	            
+	            this.importFile(evt);
+	        },
+	        
+	        /**
+	         * Determine if a key is already defined and get information about it if it does.
+	         */
+	        render: function () {
+	        	
+	        	var uri = Splunk.util.make_url("/custom/google_drive/service_account_keys/getDefaultServiceAccountKeyInfo")
+	           
+	        	jQuery.ajax({
+		             url: uri,
+		             
+		             success: function(result) {
+		            	 this.service_account_email = result['service_account_email'];
+		            	 this.filename = result['filename'];
+		            	 this._render();
+		             }.bind(this),
+		             
+		             error: function(jqXHR,textStatus,errorThrown) {
+		            	 alert("Unable to get information about the current service key");
+		             }
+	        	});
+	        },
+	        
+	        /**
+	         * Render the view contents based on the information we know about the key.
+	         */
+	        _render: function(){
+	        	
+	        	// Show the parts of the view that show that a key has been loaded
+	        	if(this.service_account_email != null){
+	        		$('.show_when_no_key').hide();
+	        		$('.show_when_with_key').show();
+	        		$('#service_account_email').text(this.service_account_email);
+	        	}
+	        	
+	        	// Show the parts of the view that show that a key has _not_ been loaded
+	        	else{
+	        		$('.show_when_no_key').show();
+	        		$('.show_when_with_key').hide();
+	        		$('#service_account_email').text("");
+	        	}
+	        	
+	        }
+	        
+	    });
+	    
+    	// Make the view instance
+    	var setup_view = new GoogleDriveSetupView({
+    		'el' : $('#google_drive_setup_view')
+    	});
+    	
+    	// Render the view
+    	setup_view.render();
+    	
+	}
+);
